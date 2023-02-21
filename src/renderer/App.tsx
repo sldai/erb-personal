bf40b7861ee126a8b944b9e810010f3d94465566
Shilong <daishilong1236@gmail.com>
2023-02-21 15:41:04 +0800

    add tailwind

---
+++ src/renderer/App.tsx
@@ -0,0 +1,26 @@
+import { MemoryRouter as Router, Route, Routes } from 'react-router-dom';
+import icon from '../../assets/icon.png';
+import AppProvider from './providers/app';
+
+const Hello = () => {
+  return (
+    <div className="text-center">
+      <img src={icon} alt="logo" className="h-48 w-48 mx-auto" />
+      <h1 className="text-2xl font-bold">
+        electron-react-boilerplate with tailwind
+      </h1>
+    </div>
+  );
+};
+
+export default function App() {
+  return (
+    <AppProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Hello />} />
+        </Routes>
+      </Router>
+    </AppProvider>
+  );
+}
